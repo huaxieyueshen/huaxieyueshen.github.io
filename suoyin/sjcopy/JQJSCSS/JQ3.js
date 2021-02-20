40648823db003435ff73e6900abdece115ed152f
花谢悦神 <45159095+huaxieyueshen@users.noreply.github.com>
2021-02-20 16:00:57 +0800

    Add files via upload

---
+++ suoyin/sjcopy/JQJSCSS/JQ3.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $("#a1").click(
+   function(){
+	$(".Ans").hide();
+});
+  $("#key1").click(function(){
+    $("#an1").show();
+  });
+  $("#key2").click(function(){
+    $("#an2").show();
+  });
+    $("#key3").click(function(){
+    $("#an3").show();
+  });
+      $("#key4").click(function(){
+    $("#an4").show();
+  });
+      $("#key5").click(function(){
+    $("#an5").show();
+  });
+      $("#key6").click(function(){
+    $("#an6").show();
+  });
+      $("#key7").click(function(){
+    $("#an7").show();
+  });
+      $("#key8").click(function(){
+    $("#an8").show();
+  });
+});
